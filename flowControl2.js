9b5db6deea7b5627d1453324fe6eef2a6253b03f
Marla Gualdron <ma.gualdron@gmail.com>
2022-09-01 16:07:00 -0300

    adding excersices

--- flowControl2.js
+++ flowControl2.js
@@ -29,19 +29,22 @@ del programa
 Solución Registro 
 */
 
-let welcome = confirm("Bienvenido, ¿Desea registrarse?");
+const welcome = confirm("Bienvenido, ¿Desea registrarse?");
 
-if (welcome === true) {
+if (welcome) {
   let userName = prompt(
     "Inserta un nombre de usuario, el mismo debe tener minimo 3 carácteres"
   );
 
-  while (userName.length < 3) {
-    userName = prompt(
-      `El nombre de usuario tiene menos de 3 caracteres, por favor ingresa un nombre de usuario más largo`
-    );
+  if (userName === null) {
+    alert("Hasta luego, vuelve pronto");
+  } else {
+    while (userName.length < 3) {
+      userName = prompt(
+        `El nombre de usuario tiene menos de 3 caracteres, por favor ingresa un nombre de usuario más largo`
+      );
+    }
   }
-
   saveUserName = userName.toLowerCase();
 
   password = prompt(
@@ -57,16 +60,14 @@ if (welcome === true) {
   login = confirm("Te has registrado correctamente, ¿deseas iniciar sesión?");
 
   if (login === true) {
-    confirmUser = prompt("Ingresa tu usuario");
+    confirmUser = prompt("Ingresa tu usuario").toLowerCase();
 
     while (confirmUser !== saveUserName) {
-      console.log("primer ingreso confirm user", confirmUser);
       confirmUser = prompt(
         "Usuario incorrecto, por favor ingresa de nuevo tu usuario"
-      );
-      console.log("segundo ingreso confirmUser", confirmUser);
+      ).toLowerCase();
     }
-  } else window.close;
+  }
 
   confirmPassword = prompt("Ingresa tu contraseña");
 
@@ -76,5 +77,107 @@ if (welcome === true) {
     );
   }
 
-  let session = alert("Sesión inciada");
-} else goodbye = alert("Gracias por visitarnos, vuelva pronto");
+  session = alert("Sesión inciada");
+} else alert("Gracias por visitarnos, vuelva pronto");
+
+// Solución startCoding
+//CODING CHALLENGE
+
+/*1) REGISTRO.
+
+  Vamos a construir un programa que permita al usuario registrarse en nuestra aplicacion. Para ello, en primer lugar vamos a
+  mostrar un mensaje que le de la bienvenida al usuario y le pregunte si desea registrarse (PISTA: recuerden que habia un metodo del 
+  navegador que permitia hacer este tipo de consultas al usuario, devolviendo true o false segun el caso).
+  En caso de que el usuario acepte registrarse, le vamos a pedir que ingrese su nombre de usuario en primer lugar. El nombre de usuario
+  tiene que tener por lo menos 3 caracteres y no va a ser case sensitive, por lo que lo vamos a almacenar en minusculas, independientemente
+  de como lo ingrese el usuario. Agregar la validacion necesaria para que en caso de que el usuario no cumpla con las concidiones, 
+  se muestre un mensaje de error.
+  Si el nombre de usuario es ingresado correctamente, le vamos a pedir al usuario que ingrese una contrasena, la que tiene que tener
+  por lo menos 6 caracteres. La contrasena tiene que ser case sensitive, por lo que se respetan las mayusculas y minusculas segun lo ingrese
+  el usuario.
+  En caso de que la contrasena no cumpla con el requisito anterior, se debe mostrar un mensaje de error. De lo contrario, se debe mostrar un
+  mensaje de exito informando al usuario que se completo el registro. (OTRA PISTA: Para ver como validar la longitud de un string, pueden ver el siguiente
+    enlace: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length )
+
+*/
+// let isRegistered, registerUserName, registerPassword;
+
+// const wishesToRegister = confirm(
+//   "Bienvenido al sitio. Haz click en OK para registrarte"
+// );
+
+// do {
+//   if (wishesToRegister) {
+//     if (!registerUserName) {
+//       registerUserName = prompt("Ingresa tu nombre de usuario");
+//     }
+
+//     if (registerUserName.length >= 3) {
+//       registerUserName = registerUserName.toLowerCase();
+
+//       registerPassword = prompt("Ingresa tu password");
+
+//       if (registerPassword.length >= 6) {
+//         alert("El registro se completo exitosamente");
+//         isRegistered = true;
+//       } else {
+//         alert("El password tiene que tener por lo menos 6 caracteres");
+//         registerPassword = null;
+//       }
+//     } else {
+//       alert("El nombre de usuario tiene que tener por lo menos 3 caracteres");
+//     }
+//   } else {
+//     alert("OK, te esperamos cuando gustes");
+//   }
+// } while (wishesToRegister && (!registerPassword || !registerUserName));
+
+/*2) LOGIN
+
+  Partiendo de la base del punto anterior, en caso de que se complete el registro, vamos a mostrarle un mensaje al usuario preguntandole
+  si desea iniciar sesion. En caso de que el usuario haga click en aceptar, vamos a pedirle que ingrese su nombre de usuario y luego su contrasena
+  Si alguno de los datos ingresados no coinciden con los informados al momento del registro, le vamos a mostrar un mensaje de error al usuario.
+  Caso contrario, le mostraremos un mensaje que diga que el login fue exitoso. Recordar el tema de normalizar los inputs en caso de que sea necesario,
+  para poder hacer la comparacion contra los valores almacenados.
+
+  */
+
+// let loginPassword, loginUsername;
+
+// if (isRegistered) {
+//   let wishesToLogin = confirm("Haz click en OK para iniciar sesion");
+
+//   do {
+//     if (wishesToLogin) {
+//       loginUsername = prompt("Ingresa tu usuario");
+
+//       if (loginUsername !== null) {
+//         loginUsername = loginUsername.toLowerCase();
+//         loginPassword = prompt("Ingresa tu password");
+
+//         if (loginPassword === null) {
+//           wishesToLogin = false;
+//         } else if (
+//           loginUsername === registerUserName &&
+//           loginPassword === registerPassword
+//         ) {
+//           alert("Login Exitoso!");
+//         } else {
+//           alert("Alguno de los datos ingresados es incorrecto");
+//           loginPassword = null;
+//           loginUsername = null;
+//         }
+//       } else {
+//         wishesToLogin = false;
+//       }
+//     } else {
+//       alert("Vuelve cuando gustes");
+//     }
+//   } while (wishesToLogin && (!loginUsername || !loginPassword));
+// }
+
+/*3) EXTRA
+  Tomando los ejercicios anteriores, los vamos a modificar para que, en el caso de que cualquiera de las validaciones anteriores arroje un error
+  el programa vuelva a pedir al usuario la informacion solicitada en ese punto. Tener en cuenta que el usuario podra dar por terminado el programa
+  tocando el boton de cancelar al momento en que se le pide ingresar un dato o se le consulta si desea registrarse o iniciar sesion.
+*/
---
+++ functions.js
@@ -0,0 +1,81 @@
+/*1.Pedir un número al usuario y devolverle si el mismo es -, + ó 0*/
+/*2.Pedir un número al usuario indicando  si el mismo es primo o no*/
+/*3.Crear un programa que convierta la temperatura de grados celsius y viceversa*/
+/*4.Crear un programa que devuelva el total de la compra, para ello se pide
+que ingrese el precio de cada producto y cuando ingrese la palabra total, se le devuelve
+el total*/
+
+// Pimer ejercicio
+
+const ansNumber = (number) => {
+  let message = "";
+  let typeNumber = +number;
+  if (!typeNumber && typeNumber !== 0) {
+    return (message = "No es un número");
+  }
+  if (typeNumber > 0) {
+    return (message = "el número ingresado es POSITIVO");
+  } else if (typeNumber < 0) {
+    return (message = "el número ingresado es NEGATIVO");
+  } else typeNumber === 0;
+  return (message = "Es CERO");
+};
+
+const askNumber = prompt("Dime que número quieres evaluar");
+
+const response = ansNumber(askNumber);
+
+alert(response);
+
+//Segundo ejercicio
+const secondNumber = prompt("Dime que número quieres evaluar si es primo o no");
+
+const primeNumber = (secondNumber) => {
+  console.log(secondNumber);
+  if (secondNumber == 2) {
+    return alert(`El numero ${secondNumber} es primo`);
+  }
+  for (let i = 2; i <= secondNumber; i++) {
+    if (secondNumber % i != 0 && secondNumber % i != 0) {
+      return alert(`El numero ${secondNumber} es primo`);
+    } else {
+      return alert(`El numero ${secondNumber} no es primo`);
+    }
+  }
+};
+primeNumber(secondNumber);
+
+//Otra forma
+
+const isPrime = (nunber) => {
+  for (let i = 2; i < nunber; i++) {
+    if (nunber % i == 0) return alert(false);
+  }
+  return alert(true);
+};
+const number = prompt("Dime que número quieres evaluar si es primo o no");
+
+isPrime(number);
+
+//Tercer ejercicio
+
+const choosingDegreeTyoe = (degreeType) => {
+  let parseDegreeType = degreeType.toLowerCase();
+  if (parseDegreeType === "c") {
+    let celsius = +prompt("Escribe el valor en celsius a convertir");
+    alert(`El resultado en farenheit es: ${(celsius * 9) / 5 + 32}`);
+  } else if (parseDegreeType === "f") {
+    let fahrenheit = +prompt("Escribe el valor en farenheit a convertir");
+    alert(`El resultado en celsius es :  ${((32 * fahrenheit - 32) * 5) / 9}`);
+  } else {
+    return alert("No escribiste ni c, ni f");
+  }
+};
+
+const degreeType = prompt(
+  "¿Deseas convertir celcius (c) o frarenheit (f)? Por favor escribe sólo c ó f"
+).toLowerCase();
+
+const checkDegreeType = choosingDegreeTyoe(degreeType);
+
+// Cuarto Ejercicio
